fa376f3656de21f5944ecd1f1145e168a45d1edc
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-07-06 13:39:25 +0200

    Initial commit

---
+++ hpc/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Baner from "./components/baner/baner";
+
+function App() {
+  return <Baner />;
+}
+
+export default App;
---
+++ hpc/src/components/baner/baner.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Baner() {
+  return (
+    <div>
+      <h1>Porównywarka Pomp Ciepła</h1>
+    </div>
+  );
+}
+
+export default Baner;
