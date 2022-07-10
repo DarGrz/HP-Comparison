ffcb42bafb137c84a043a1ccb4cd4cabcd85f068
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-07-10 16:23:03 +0200

    Added input useState

--- hpc/src/components/SearchForm.js
+++ hpc/src/components/SearchForm.js
@@ -1,24 +1,68 @@
-import react from "react";
+import react, { useState } from "react";
 import classes from "./SearchForm.module.css";
 import Button from "./Button";
 
 const SearchForm = () => {
+  const [searchProducent, setSearchProducent] = useState("");
+  const [searchModel, setSearchModel] = useState("");
+  const [searchPower, setSearchPower] = useState("");
+  const [searchType, setSearchType] = useState("");
+
+  const searchHandler = (e) => {
+    e.preventDefault();
+    console.log(searchProducent, searchModel, searchPower, searchType);
+    setSearchProducent("");
+    setSearchModel("");
+    setSearchPower("");
+  };
+
+  const producentSearchHandler = (event) => {
+    setSearchProducent(event.target.value);
+  };
+  const modelSearchHandler = (e) => {
+    setSearchModel(e.target.value);
+  };
+  const powerSearchHandler = (e) => {
+    setSearchPower(e.target.value);
+  };
+  const typeSearchHandler = (e) => {
+    setSearchType(e.target.value);
+  };
+
   return (
     <div className={classes.searchform}>
-      <form id="searchform">
+      <form onSubmit={searchHandler} id="searchform">
         <label htmlFor="producent">Producent</label>
-        <input type="text" id="producent"></input>
+        <input
+          value={searchProducent}
+          type="text"
+          id="producent"
+          onChange={producentSearchHandler}
+        ></input>
+
         <label htmlFor="model">Model</label>
-        <input type="text" id="model"></input>
-        <label htmlFor="power">Moc w kW</label>
-        <input type="text" id="power"></input>
+        <input
+          value={searchModel}
+          type="text"
+          id="model"
+          onChange={modelSearchHandler}
+        ></input>
+
+        <label htmlFor="power">Moc (kW)</label>
+        <input
+          value={searchPower}
+          type="text"
+          id="power"
+          onChange={powerSearchHandler}
+        ></input>
+
         <label htmlFor="type">Typ</label>
-        <select id="type" form="searchform">
+        <select id="type" form="searchform" onChange={typeSearchHandler}>
           <option value="Split">Split</option>
           <option value="Monoblok">Monoblok</option>
         </select>
+        <Button type="submit">Wyszukaj</Button>
       </form>
-      <Button type="submit">Wyszukaj</Button>
     </div>
   );
 };
